6d1877031fdf1894f680f62ce1264896ffe3c8f9
dnkkhan18 <130484697+dnkkhan18@users.noreply.github.com>
2025-01-02 19:42:32 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,21 +8,198 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(child: Scaffold(
-        backgroundColor: Colors.green,
-        body: Center(
-          child: Image.asset('images/Frame.png'),
-        ),
-        
-        )
-      )
-
-    
+      home: SplashScreen(),
     );
   }
 }
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => First()),
+      );
+    });
+
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Image.asset('images/Frame.png'),
+      ),
+    );
+  }
+}
+
+class First extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 243, 177, 194),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset('images/Picture1.png'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Tiện Ích',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                  'Tối ưu hóa trải nghiệm người đùng với các công nghệ hiện đại',
+                  style: TextStyle(fontSize: 17)),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Second()),
+                  );
+                },
+              
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "Tiếp",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(width: 8),
+                    const Icon(Icons.arrow_forward, color: Colors.white),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class Second extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 243, 177, 194),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset('images/Picture2.png'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Thuận Lợi',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Dễ dàng tiếp cận và sử dụng mọi lúc, mọi nơi',
+                style: TextStyle(fontSize: 17),
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Third()),
+                  );
+
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "Tiếp",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(width: 8),
+                    const Icon(Icons.arrow_forward, color: Colors.white),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Third extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 243, 177, 194),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Image.asset('images/Picture3.png'),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Chủ động',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Kiểm soát thời gian và lịch trình của bạn hiệu quả hơn',style: TextStyle(fontSize: 17)
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {},
+                child: const Text("ĐĂNG KÝ",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  Text('Bạn đã có tài khoản?'),
+                  SizedBox(width: 5),
+                  Text('Đăng nhập', style: TextStyle(color: Colors.green)),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
 
